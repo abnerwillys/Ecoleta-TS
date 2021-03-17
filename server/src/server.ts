56db9418a83d2a79fd9cd64a13afeafbc6b6c1b5
Abner Willys <abner.rodrigues@stone.com.br>
2021-03-17 02:09:09 -0300

    knex configuration, migrations and db created

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,9 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-    console.log('hello')
-    response.json([
-        'Abner',
-        'Mariana',
-        'Rayka',
-        'Astolfo',
-        'Lunna',
-        'Hadassa'
-    ])
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333)
